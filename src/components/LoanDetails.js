83773730f0ef7eef6dcc03f5e2419f2aacda459a
Titus Goh <titusgoh@Mac.modem>
2025-10-15 05:21:43 +1100

    bugs

--- src/components/LoanDetails.js
+++ src/components/LoanDetails.js
@@ -434,6 +434,7 @@ export default function LoanDetails() {
                       })()
                     }`}
                   >
+                    <option value="-"> </option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
